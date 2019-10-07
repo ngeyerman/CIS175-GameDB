e7543426693c064b10c35d94c763fe1bee9da3e7
ngeyerman <54454589+ngeyerman@users.noreply.github.com>
2019-10-06 22:05:34 -0500

    starting with model and controller options

--- src/NewProject.java
+++ src/NewProject.java
@@ -1,5 +1,5 @@
 
-public class NewProject {
+/*public class CIS175-GameDB {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -7,3 +7,4 @@ public class NewProject {
 	}
 
 }
+*/
